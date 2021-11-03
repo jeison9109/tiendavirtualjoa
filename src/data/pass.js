b00f6632486d4108c281479d13f14994513fbb02
Yeison Serna <yeisonivanserna@gmail.com>
2021-11-03 12:09:11 -0500

    passwords apis

---
+++ src/data/pass.js
@@ -0,0 +1,7 @@
+const pass = {
+  paypalPaymentClientID:
+    "ARU3O4EGDglvZWZDdtHTUxfT22qGSyHZ0afmkazYWT3AKT6ycudbxJTs4dGMk4mMlqSqdu1hO_yoFT_N",
+  googleMapsAPI: "AIzaSyB6vZ8m82pSzFhR-fmFK4is5pl3KuAwmM8",
+};
+
+export default pass;
